99701d8935002151fc00ffd06ad09f665749f722
Roman Ehrbar <roman.ehrbar@mgb.ch>
2024-05-06 12:47:36 +0000

    improve prototype

--- intergalactic/src/middleware.ts
+++ intergalactic/src/middleware.ts
@@ -6,7 +6,8 @@ export async function onRequest({ locals, request }, next) {
     // return a Response or the result of calling `next()`
     const resp = await next();
     console.log("resp", resp);
-    resp.headers.set("Content-Security-Policy","default-src 'self'; frame-src romanehrbar.ch; report-to my-reports; report-uri /csp")
+    //resp.headers.set("Content-Security-Policy","default-src 'self'; frame-src romanehrbar.ch; report-to my-reports; report-uri /csp")
+    // Report-To is not supported in meta element.
     resp.headers.set("Report-to", JSON.stringify({
         "group": "my-reports",
         "max_age": 10886400,
